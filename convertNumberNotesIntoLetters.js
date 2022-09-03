a4bdd2c72d4bb9f1a211acf15d9f6d0e18e7b771
Thay7 <thay3x@gmail.com>
2022-09-03 19:52:52 -0300

    first commit

---
+++ convertNumberNotesIntoLetters.js
@@ -0,0 +1,40 @@
+function notaEmLetra(score) {
+
+    let scoreA = score >= 90 && score <= 100
+    let scoreB = score >= 80 && score  <= 89
+    let scoreC = score >= 70 && score <= 79
+    let scoreD = score >= 60 && score <= 69
+    let scoreF = score < 60 && score >= 0
+
+    let scoreFinal
+
+    if (scoreA) {
+
+        scoreFinal = 'A'
+
+    } else if (scoreB) {
+
+        scoreFinal = 'B'
+
+    } else if (scoreC) {
+
+        scoreFinal = 'C'
+
+    } else if (scoreD) {
+
+        scoreFinal = 'D'
+
+    } else if (scoreF) {
+
+        scoreFinal = 'F'
+
+    } else {
+
+        scoreFinal = 'Nota inválida! Insira uma nota de 0 a 100!'
+
+    }
+
+    return scoreFinal
+} 
+
+console.log(notaEmLetra(100))
