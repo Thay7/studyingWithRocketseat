0dc8cf0798ad1176c8b343b111960858484faaa5
Thay7 <thay3x@gmail.com>
2022-09-09 23:16:00 -0300

    update

--- fetchingDataInArray.js
+++ fetchingDataInArray.js
@@ -35,4 +35,30 @@ const booksByCategory = [
     },
 ];
 
-//contar o numero de categorias e o número de livros em cada categoria teste
+//1. Contar o numero de categorias e o número de livros em cada categoria
+
+const numeroDeCategorias = booksByCategory.length //acessando tamanho do array e guardando o valor na variavel
+
+const numeroDeLivros = booksByCategory[0].books.length //acesssando 
+
+console.log(`O numero total de categorias é ${numeroDeCategorias} e o número total de livros em cada categoria é ${numeroDeLivros}`)
+
+//2. Contar o número de autores
+
+const numeroDeAutores = booksByCategory[0].books.length
+
+console.log(`O número total de autores é ${numeroDeAutores}`)
+
+//3. Mostrar livros do autor Augusto Curry
+
+function booksByCurry() {
+
+    for (let title of booksByCategory[1].books) {
+
+        if (title.author === 'Augusto Curry') {
+            console.log(`Os livros são: ${title.title}`)
+        }
+    }
+}
+
+booksByCurry()
