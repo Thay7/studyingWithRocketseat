f89f85ffb18abdfbe64254233f177a804c1ef157
Thay7 <thay3x@gmail.com>
2022-09-10 12:14:43 -0300

    update

--- fetchingDataInArray.js
+++ fetchingDataInArray.js
@@ -41,24 +41,40 @@ const numeroDeCategorias = booksByCategory.length //acessando tamanho do array e
 
 const numeroDeLivros = booksByCategory[0].books.length //acesssando 
 
-console.log(`O numero total de categorias é ${numeroDeCategorias} e o número total de livros em cada categoria é ${numeroDeLivros}`)
+//console.log(`O numero total de categorias é ${numeroDeCategorias} e o número total de livros em cada categoria é ${numeroDeLivros}`)
 
 //2. Contar o número de autores
 
 const numeroDeAutores = booksByCategory[0].books.length
 
-console.log(`O número total de autores é ${numeroDeAutores}`)
+//console.log(`O número total de autores é ${numeroDeAutores}`)
 
 //3. Mostrar livros do autor Augusto Curry
 
-function booksByCurry() {
+// function booksByCurry() { //inicialização da função
 
-    for (let title of booksByCategory[1].books) {
+//     for (let book of booksByCategory[1].books) { //"for of" para pegar os elementos da posição 1, do elemento "books" do array "booksByCategory" 
 
-        if (title.author === 'Augusto Curry') {
-            console.log(`Os livros são: ${title.title}`)
+//         if (book.author === 'Augusto Curry') { //se o autor dos elementos que estão armazenados na variavel de controle for 'Augusto Curry' entra aqui
+
+//             console.log(`Os livros são: ${book.title}`) //imprime o nome do livro
+//         }
+//     }
+// }
+
+//booksByCurry()
+
+//4. Transformar a função acima em uma função que irá receber o nome do autor e devolver os livros desse autor
+
+function booksByAuthor(author) { //inicialização da função
+
+    for (let book of booksByCategory[0].books) { //"for of" para pegar os elementos da posição 1, do elemento "books" do array "booksByCategory" 
+
+        if (book.author === author) { //se o autor dos elementos que estão armazenados na variavel de controle for 'Augusto Curry' entra aqui
+
+            console.log(`Os livros de ${author} são: ${book.title}`) //imprime o nome do livro
         }
     }
 }
 
-booksByCurry()
+booksByAuthor('George S. Clason')
