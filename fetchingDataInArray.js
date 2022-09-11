d7b3e9c94f137e1bba2b8140b9fc0c5d94b9d5bf
Thay7 <thay3x@gmail.com>
2022-09-11 00:21:38 -0300

    update

--- fetchingDataInArray.js
+++ fetchingDataInArray.js
@@ -37,14 +37,14 @@ const booksByCategory = [
 
 //1. Contar o numero de categorias e o número de livros em cada categoria
 
-// const totalCategories = booksByCategory.length //acessando tamanho do array e guardando o valor na variavel
+const totalCategories = booksByCategory.length //acessando tamanho do array e guardando o valor na variavel
 
-// console.log(`O numero total de categorias é ${totalCategories}`) //saída do tamanho do array
+console.log(`O numero total de categorias é ${totalCategories}`) //saída do tamanho do array
 
-// for(let category of booksByCategory) { //"for of" para percorrer todo o array, guardando na variavel "category"
+for(let category of booksByCategory) { //"for of" para percorrer todo o array, guardando na variavel "category"
 
-//      console.log('Total de livros da categoria: ', category.category ,'é', category.books.length) //saída acessando nome da categoria do objeto e o número de livros com o .lenght 
-// }
+     console.log('Total de livros da categoria: ', category.category ,'é', category.books.length) //saída acessando nome da categoria do objeto e o número de livros com o .lenght 
+}
 
 //2. Contar o número de autores
 
@@ -68,35 +68,47 @@ function countAuthors() {
 
 countAuthors()
 
-//console.log(`O número total de autores é ${numeroDeAutores}`)
+console.log(`O número total de autores é ${numeroDeAutores}`)
 
 //3. Mostrar livros do autor Augusto Curry
 
-// function booksByCury() { //inicialização da função
+function booksOfCury(author) { 
 
-//     for (let book of booksByCategory[1].books) { //"for of" para pegar os elementos da posição 1, do elemento "books" do array "booksByCategory" 
+    let books = [] //array vazio que irá receber os titulos dos livros
 
-//         if (book.author === 'Augusto Cury') { //se o autor dos elementos que estão armazenados na variavel de controle for 'Augusto Cury' entra aqui
+    for (let category of booksByCategory) { //loop para percorrer as categorias do array
 
-//             console.log(`Os livros são: ${book.title}`) //imprime o nome do livro
-//         }
-//     }
-// }
+         for (let book of category.books) { //loop para percorrer os livros do array
 
-//booksByCurry()
+             if (book.author === author) { //condicional que verifica se o author do livro é igual ao argumento passado
+
+                books.push(book.title) //se for, coloca o nome do livro dentro do array
+        }
+    }
+}
+
+console.log(`Os livros de ${author} são: ${books.join(", ")}`)//saída no console dos nomes dos livros que entraram no array (.join transforma a variavel em um array e me permite colocar um separador)
+
+}
+
+booksOfCury('Augusto Cury')
 
 //4. Transformar a função acima em uma função que irá receber o nome do autor e devolver os livros desse autor
 
-// function booksByAuthor(author) { //inicialização da função
+function booksOfAuthor(author) { //inicialização da função
 
-//     for (let book of booksByCategory) { //"for of" para pegar os elementos da posição 1, do elemento "books" do array "booksByCategory" 
-    
-//          if (booksByCategory[0].books[0].author === author || booksByCategory[1].books[0].author === author) { //se o autor dos elementos que estão armazenados na variavel de controle for 'Augusto Curry' entra aqui
+    let books = []
 
-//              console.log(`Os livros de  são: ${book.books[0].title}`) //imprime o nome do livro do autor especifico
-//          }    
-//     }
-// }
+    for (let category of booksByCategory) { //"for of" para pegar a categoria do array
 
-//booksByCategory.forEach(booksByAuthor)
-// (booksByAuthor('Augusto Cury'))
+        for (let book of category.books) { //"for of" para acessar os livros da categoria
+
+            if (book.author === author) { //condicional para verificar se o autor passado como argumento é igual ao do livro
+                books.push(book.title) //se for, ele guarda o nome do livro dentro do array
+            }
+        }
+     }    
+         console.log(`Livro(s) de ${author}: ${books.join(", ")}`) //imprime o(s) nome(s) do(s) livro(s) do autor o seu proprio
+}
+
+(booksOfAuthor('George S. Clason')) //chamando a função e passando o argumento "autor"
