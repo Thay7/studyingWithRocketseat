bd569fbeae03a42bac537516209dc3fa3fa87088
Thay7 <thay3x@gmail.com>
2022-09-12 09:02:11 -0300

    updates

---
+++ Javascript/aula2.js
@@ -0,0 +1,43 @@
+let weight;                  
+console.log(typeof weight); //questão 1
+
+let name = 'Thay';
+let age = 21;
+let stars = 6.5;
+let isSubscribed = true; //questão 2
+
+/* let student = {}
+
+console.log(typeof student) */ //questão 3
+
+let student = {
+
+    name: 'Thay',
+    age: 21,
+    weight: 55.4,
+    isSubscribed: true
+
+}
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} kg.`); //questão 4
+
+ students = [
+    student
+];            
+console.log(students[0])  //questão 5
+
+const john = {
+    name: jhon,
+    age: 21,
+    weight: 70,
+    isSubscribed: true
+}
+
+students[1] = jhon;
+
+console.log(students[1]); //8
+
+console.log(a)
+a = 1;  // vai dar: undefined por causa do hoisting 
+//9
+
+
---
+++ Javascript/functions.js
@@ -0,0 +1,9 @@
+function createPrashes() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é a mãe do aprendizado')
+}
+
+createPrashes()
+
+console.log('Fim do programa')
