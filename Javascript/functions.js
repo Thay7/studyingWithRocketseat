58cbcc9e2a3c13e6293f64cf39ac69ed40a1dd43
Thay <67573675+Thay7@users.noreply.github.com>
2022-09-12 09:03:18 -0300

    Delete functions.js

--- Javascript/functions.js
+++
@@ -1,9 +0,0 @@
-function createPrashes() {
-    console.log('Estudar é muito bom')
-    console.log('Paciência e persistência')
-    console.log('Revisão é a mãe do aprendizado')
-}
-
-createPrashes()
-
-console.log('Fim do programa')
