f8e0c7d4a0a960af08342a2bfde9ec056313f851
Thay7 <thay3x@gmail.com>
2022-09-16 12:47:07 -0300

    update

--- Javascript/Events/modal.js
+++ Javascript/Events/modal.js
@@ -3,22 +3,22 @@
 
 const buttonOpenModal = document.getElementById('openModal') //pegando o botão pelo id e armazenando numa variavel
 
-const modalWrapper = document.querySelector('.modal-wrapper') //pegando a classe que tem 
+const modalWrapper = document.querySelector('.modal-wrapper') //pegando a div "tecle esc para fechar" pela classe 
 
-buttonOpenModal.onclick = function () { //pega o botão e adiciona o evento de clique e atribui a funcionalidade
+//adicionando elemento de clique no botão
+buttonOpenModal.onclick = function () { //pega o botão e adiciona o evento de clique, atribuindo ao modalWrapper a funcionalidade  de remover a classe "invisible"
 
     modalWrapper.classList.remove('invisible')//pega a classe modal-wrapper, da lista de classe e remove a classe chamada "invisible"
 
-} //adicionando elemento de clique no botão
+} 
 
-//adicionar o momento de fechar a janela
+//adicionando o momento de fechar a janela
+document.addEventListener('keydown', function(event) { //o ESC lê na tela inteira, então é adicionado o document e o evento de esperar a tecla ser pressionada para fechar a janela (passando o event pois precisar acessar o prototype para veriifcar se a tecla digitada é "ESC")  
 
-document.addEventListener('keydown', function (event) {
+    const isEscKey = event.key === 'Escape' //vai acessar o event.key no prototype e verificar se é igual a Escape
 
-    const isEscKey = event.key === 'Escape'
-
-    if(isEscKey) {
-        modalWrapper.classList.add('invisible')
+    if(isEscKey) { //se isEscKey for true realiza a ação abaixo
+        modalWrapper.classList.add('invisible') //a classe "invisible" é adicionada novamente no modalwrapper e a janela é fechada
     }
 
 })
