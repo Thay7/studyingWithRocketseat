3020b171680b139b27edca0a5fe2f7f1e800bea7
Thay7 <thay3x@gmail.com>
2022-09-25 12:24:09 -0300

    add app

---
+++ Javascript/node/questionsApp.js
@@ -0,0 +1,34 @@
+const questions = [ //variavel com um array de perguntas
+
+    "O que aprendi hoje?",
+    "O que me deixou aborrecido? E o que eu poderia fazer para melhorar?",
+    "O que eu poderia fazer para melhorar?",
+    "O que me deixou feliz hoje?",
+    "Quantas pessoas ajudei hoje?"
+]
+
+const ask = (index = 0) => { //função que irá fazer as perguntas
+
+    process.stdout.write(questions[index] + "\n") //processo de saída no terminal quando executamos o app
+}
+
+ask() //chamando a função
+
+const answers = [] //criando array que receberá as respostas 
+
+//interação com o app pelo terminal, utilizando process.stdin
+
+process.stdin.on("data", data => { //On é o listener para receber os dados digitados no terminal. Em seguida temos a função data que será rodada todas as vezes que forem inseridos dados no processo
+
+    answers.push(data.toString().trim()) //irá colocar a resposta no array, o toString transforma o dado em string e o trim remove os espaços vazios do começo e fim
+
+    if (answers.length < questions.length) { //se o numero de respostas for menor que o numero de perguntas
+
+        ask(answers.length) //ao entrar aqui a primeira vez, o ask(index) vai ser 1 e assim sucessivamemnte. então quando voltar ao inicio vai para a pergunta seguinte 
+
+    } else { //quando o tamanho do array de respostas for igual ao tamanho do array de perguntas 
+
+        console.log(answers) //escreva o array de respostas no console
+        process.exit() //e encerre o processo
+    }
+})
