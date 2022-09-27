02637a286645f5ab153c4c19c9aca7b579759e7a
Thay7 <thay3x@gmail.com>
2022-09-27 17:03:04 -0300

    update ejs

--- Javascript/Node.js/Node+EJS/server.js
+++ Javascript/Node.js/Node+EJS/server.js
@@ -1,4 +1,4 @@
-const express = require('express')//cria um servidor para mostrar oq eutou construindo no navegador
+const express = require('express')//chamando o express que cria um servidor para mostrar oq eutou construindo no navegador
 
 const app = express()// instanciando o express e guardando na variavel app
 
@@ -7,14 +7,45 @@ app.set("view engine", "ejs")//falando pro express que usarei o ejs como ferrame
 
 
 app.get("/", function (req, res) { //criando rota referente ao nome do meu site "/"
-    res.render("index") //vou dar de resposta para essa rota: renderizar o index
+    const items = [
+        {
+            title: "D",
+             message: "Desenvolver aplicações/serviços de forma fácil"
+        },
+        {
+            title: "E",
+            message: "EJS usa Javascrip para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "Muito fácil de usar"
+        },
+        {
+            title: "A",
+            message: "Amorzinho"
+        },
+        {
+            title: "I",
+            message: "install ejs"
+        },
+        {
+            title: "S",
+            message: "Simples"
+        }
+    ]
+
+    res.render("pages/index", {
+        qualitys: items, 
+    }) //vou dar de resposta para essa rota: renderizar o index
 }) 
 
 app.get("/sobre", function (req, res) { //rota para pagina about
-    res.render("about")
+    res.render("pages/about")
 })
 
 app.listen(8080) //app carrega tudo do express e o listen é uma funçao do express que fica ouvindo uma porta, no caso a minha é 8080
 //quando colocar no navegador  localhost:8080 o app irá rodar
 
 console.log("Rodando")
+
+//criar pasta chamada views e colocar o index nessa pasta
